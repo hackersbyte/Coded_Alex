c0814eabf5aaac0ade4fd4c746b4cc3418bd4ae1
hackersbyte <alexmutonga3@gmail.com>
2024-02-20 02:25:41 +0000

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,11 +2,15 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv  from 'dotenv';
 import userRoutes  from './routes/user.route.js'
+import authRoutes  from './routes/auth.route.js'
 
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO)
+  .connect(process.env.MONGO , {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then ( ()  => {
     console.log('MongoDb is connected');
   })
@@ -16,8 +20,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
 
 app.use('/api/user', userRoutes );
+app.use('/api/auth', authRoutes );
