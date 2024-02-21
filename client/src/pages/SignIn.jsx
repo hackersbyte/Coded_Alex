1af300c1766ff1a5cf274f453b54ed5788d93f0a
hackersbyte <alexmutonga3@gmail.com>
2024-02-21 23:03:35 +0000

    add git toolkit and functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,27 +1,46 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
 import {Link, useNavigate }  from "react-router-dom";
-// import {useDispatch, useSelector } from 'react-redux';
-// import {
-//   signInStart,
-//   signInSuccess,
-//   signInFailure,
-// } from '../redux/user/userSlice'
+import {useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from '../redux/user/userSlice'
 
 export default function SignIn() {
-  const { loading, error: errorMessage } = ((state) => state.user);
-  // const navigate = useNavigate();
-  // const dispatch = useDispatch();
   const [formData, setFormData] = useState({});
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!formData.email ||  !formData.password) 
-    return console.error( "Form is not filled out completely" );
-  }
+    if (!formData.email || !formData.password) {
+      return dispatch(signInFailure('Please fill all the fields'));
+    }
+    try {
+      dispatch(signInStart());
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+      }
+
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate('/');
+      }
+    } catch (error) {
+      dispatch(signInFailure(error.message));
+    }
+  };
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5' >
@@ -78,7 +97,7 @@ export default function SignIn() {
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>Dont Have an account?</span>
-            <Link to='/sign-up' className='text-blue-500'>
+            <Link to='/signup' className='text-blue-500'>
               Sign Up
             </Link>
           </div>
