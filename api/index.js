4ef0da851c4f55c709a7d615188adecfb5ffd58c
hackersbyte <alexmutonga3@gmail.com>
2024-02-21 03:20:26 +0000

    create error handlers

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv  from 'dotenv';
 import userRoutes  from './routes/user.route.js'
 import authRoutes  from './routes/auth.route.js'
+import errorHandler from './utils/error.js'
 
 dotenv.config();
 
@@ -28,3 +29,14 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoutes );
 app.use('/api/auth', authRoutes );
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  
+  res.status(statusCode).json({
+      success : false,
+      statusCode,
+      message 
+  });
+})
